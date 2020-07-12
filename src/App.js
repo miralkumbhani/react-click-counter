3b4f8e6b0946a826813cb4eaa893e19f5398f5c7
miralkumbhani <kumbhanimiral@gmail.com>
2020-07-12 23:45:10 +0530

    dev-1.1: Added test cases and developed the click counter from incrementing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { Button } from "react-bootstrap";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter: 0,
+    };
+  }
+
+  render() {
+    return (
+      <div className="App text-center" data-test="component-app">
+        <h1 data-test="counter-display">
+          The value of the counter display is {this.state.counter}
+        </h1>
+        <div className="counter-buttons">
+          <Button variant="primary"
+            data-test="counter-increment-button"
+            onClick={() => this.setState({ counter: this.state.counter + 1 })}
+          >
+            Increment count
+          </Button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
